7666bd0306d4da0e84e9996751b8e158bb550999
Hsuan-Li Chen <franky+gpg@frankychen.net>
2024-02-17 14:34:35 +0800

    Add amplify

--- web/src/components/TopNav.tsx
+++ web/src/components/TopNav.tsx
@@ -1,28 +1,15 @@
 "use client"
 import {TopNavigation} from "@cloudscape-design/components";
-import React, {useLayoutEffect, useState} from "react";
-import {useRouter} from "next/navigation";
+import React from "react";
 
 export function TopNav() {
-	const router = useRouter()
 
 	return (
 		<TopNavigation
 			identity={{
 				href: '#',
 				title: 'DMARC',
-				// logo: {src: logo, alt: 'Service name logo'},
 			}}
-			// search={
-			// 	<Input
-			// 		ariaLabel="Input field"
-			// 		clearAriaLabel="Clear"
-			// 		value={searchValue}
-			// 		type="search"
-			// 		placeholder="Search"
-			// 		onChange={({detail}) => setSearchValue(detail.value)}
-			// 	/>
-			// }
 			utilities={[
 
 			]}
